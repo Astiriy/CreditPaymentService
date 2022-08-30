4c36f31fcd2703fd81df283ae765aed7049a8442
Ореолов Евгений <evoreo@yandex.ru>
2022-08-30 21:01:43 +0300

    modified:   src/CreditPaymentService.java 	modified:   src/Main.java

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,8 +1,8 @@
 public class CreditPaymentService {
-    public int calculate (int sum, int years, double percent) {
+    public int calculate(int sum, int years, double percent) {
         double monPercent = percent / 100 / 12;
         int months = (years * 12);
-        int result = ((int) (sum * (monPercent + (monPercent / (Math.pow(1 + monPercent,months)-1)))));
+        int result = ((int) (sum * (monPercent + (monPercent / (Math.pow(1 + monPercent, months) - 1)))));
         return result;
-        }
     }
+}
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         CreditPaymentService service = new CreditPaymentService();
         System.out.println("Сумма кредита: 1000000   Срок кредитования 1 год   Процентная ставка 9.99%");
         System.out.println("Ежемесячный платеж составляет: " + service.calculate(1000000, 1, 9.99));
-        System.out.println( );
+        System.out.println();
         System.out.println("Сумма кредита: 1000000   Срок кредитования 2 года   Процентная ставка 9.99%");
         System.out.println("Ежемесячный платеж составляет: " + service.calculate(1000000, 2, 9.99));
         System.out.println();
